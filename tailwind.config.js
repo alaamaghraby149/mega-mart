22c5037f00879d5d880ed41dcc9560d15b792495
AlaaMaghraby <loaymanlo13@gmail.com>
2026-03-26 22:39:36 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'1024px',
+      
+    },
+    extend: {
+      colors : {
+        primaryColor: '#008ECC',
+        textColor: "#666666",
+        headingColor: '#222222',
+        lightColor: '#888888',      
+        borderColor: '#EDEDED',
+        firstBackground: "#F5F5F5",
+        secondBackground: "#F6F6FC",
+        thirdBackground: "#F3F9FB",
+        yellow: "#E3BC01",
+        white: "#FFFFFF",
+        lineColor: "#D9D9D9",
+      },
+    },
+    container: {
+      center:true,
+      padding:{
+        DEFAULT:'7.5rem',
+      }
+    },
+    fontFamily : {
+      HostGrotesk:['Host Grotesk','sans-serif'],
+      dmsans: ['DM Sans','sans-serif']
+    }
+  },
+  plugins: [],
+}
+
