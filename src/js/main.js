e24b61c6375188cea76cb246582333cc7fcc5a6b
AlaaMaghraby <loaymanlo13@gmail.com>
2026-03-29 23:13:16 +0200

    addind slider

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  
+  slidesPerView: 1, 
+  spaceBetween: 0,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
